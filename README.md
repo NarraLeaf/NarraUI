695cf3f2087a674363aca79df003b6973500f15e
helloyork <helloyork@icloud.com>
2025-04-23 18:32:47 -0700

    update: basic information

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center"><img src="docs/nlr-logo-banner.png"></p>
+
+# NarraUI
+
+NarraUI is a React UI library for building user interfaces for NarraLeaf apps. It provides a set of reusable components and hooks that can be used to create NarraLeaf apps quickly and easily.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hello, world!")
+/*!
+ *       ___           ___           ___           ___           ___           ___       ___           ___           ___
+ *      /\__\         /\  \         /\  \         /\  \         /\  \         /\__\     /\  \         /\  \         /\  \
+ *     /::|  |       /::\  \       /::\  \       /::\  \       /::\  \       /:/  /    /::\  \       /::\  \       /::\  \
+ *    /:|:|  |      /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/  /    /:/\:\  \     /:/\:\  \     /:/\:\  \
+ *   /:/|:|  |__   /::\~\:\  \   /::\~\:\  \   /::\~\:\  \   /::\~\:\  \   /:/  /    /::\~\:\  \   /::\~\:\  \   /::\~\:\  \
+ *  /:/ |:| /\__\ /:/\:\ \:\__\ /:/\:\ \:\__\ /:/\:\ \:\__\ /:/\:\ \:\__\ /:/__/    /:/\:\ \:\__\ /:/\:\ \:\__\ /:/\:\ \:\__\
+ *  \/__|:|/:/  / \/__\:\/:/  / \/_|::\/:/  / \/_|::\/:/  / \/__\:\/:/  / \:\  \    \:\~\:\ \/__/ \/__\:\/:/  / \/__\:\ \/__/
+ *      |:/:/  /       \::/  /     |:|::/  /     |:|::/  /       \::/  /   \:\  \    \:\ \:\__\        \::/  /       \:\__\
+ *      |::/  /        /:/  /      |:|\/__/      |:|\/__/        /:/  /     \:\  \    \:\ \/__/        /:/  /         \/__/
+ *      /:/  /        /:/  /       |:|  |        |:|  |         /:/  /       \:\__\    \:\__\         /:/  /
+ *      \/__/         \/__/         \|__|         \|__|         \/__/         \/__/     \/__/         \/__/
+ *
+ * NarraUI https://github.com/NarraLeaf/NarraUI
+ * © 2025 Nomen (helloyork)
+ * a React UI library for building user interfaces for NarraLeaf apps
+ *
+ * @author: Nomen (helloyork) https://github.com/helloyork
+ * @license: MIT
+ */
+/*!
+ * NarraLeaf-React https://github.com/NarraLeaf/narraleaf-react
+ * © 2025 Nomen (helloyork)
+ * Make your own visual novel using a lightweight front-end visual novel framework.
+ *
+ * @author: Nomen (helloyork) https://github.com/helloyork
+ * @license: MPL-2.0
+ */
